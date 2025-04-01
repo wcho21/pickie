133a82b2eab227f4d1afe36373cb4287de7fb33c
Cho <wcho.dev@gmail.com>
2025-04-01 09:26:55 +0000

    fix: make session_state seen in scope

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+from langchain.memory import ConversationSummaryBufferMemory
 from content_authed import render_content_authed
 from content_not_authed import render_content_not_authed
+from langchain_openai import ChatOpenAI
+
+# initialize
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+model = ChatOpenAI(model_name="gpt-4o-mini", api_key=openai_api_key, temperature=0.6, streaming=True)
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+if "conversation_memory" not in st.session_state:
+    st.session_state["conversation_memory"] = ConversationSummaryBufferMemory(llm=model, max_token_limit=200, return_messages=True)
+conversation_memory = st.session_state["conversation_memory"]
 
 st.set_page_config(
     page_title="Pickie",
@@ -14,6 +25,6 @@ if "authed" not in st.session_state:
 authed = st.session_state["authed"]
 
 if authed:
-    render_content_authed()
+    render_content_authed(conversation_memory, model)
 else:
     render_content_not_authed()
