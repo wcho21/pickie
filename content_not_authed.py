f277ddf3997a767ad3294867f19486d2295ffeb8
Cho <wcho.dev@gmail.com>
2025-04-01 09:05:29 +0000

    feat: implement sign-in page

--- content_not_authed.py
+++ content_not_authed.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
+password = st.secrets["PASSWORD"]
+
 def render_content_not_authed():
     st.write("Hello, you need to sign in.")
 
-    st.text_input("Enter the password.", type="password")
+    claimed = st.text_input("Enter the password.", type="password")
+
+    if claimed == "":
+        return
+
+    if password == claimed:
+        st.session_state["authed"] = True
+        st.rerun()
+    else:
+        st.error("Wrong.")
